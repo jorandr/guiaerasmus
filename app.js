c9e0895f44f3ef5377a09cd6c876049c89651411
Jorge Andrés Nieto <74513985+jorandr@users.noreply.github.com>
2022-06-22 11:38:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 
 //definir puerto de escucha
-app.listen(3000,function(){
+app.listen(3001,function(){
     console.log("Buongiorno, escuchando en puerto 3000");
 });
 
