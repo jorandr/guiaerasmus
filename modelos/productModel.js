a7a88fe0900cd3e7fcb7af80999f0358fcd0489f
Jorge Andrés Nieto <74513985+jorandr@users.noreply.github.com>
2022-06-28 19:51:45 +0200

    Update productModel.js

--- modelos/productModel.js
+++ modelos/productModel.js
@@ -7,7 +7,7 @@ var productSchema=new Schema({
     description:String,
     name:String,
     url:String,
-    market:String
+    market:String,
     idCategory:{type:Schema.Types.ObjectId,ref:'Category'}
 });
 
